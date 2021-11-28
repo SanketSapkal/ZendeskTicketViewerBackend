ce0ed2f62baede30ff1e01f1870e1b60cf5f54cf
Sanket Sapkal <sanket@cursorinsight.com>
2021-11-28 15:56:57 -0800

    Add endpoint skeleton.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // Redirect to first page of tickets
+    res.redirect('/ticketsOnPage/1');
+});
+
+// GET the ticket with specified ticketId.
+router.get('/ticket/:ticketId', function (req, res) {
+    const ticketId = parseInt(req.params.ticketId)
+    res.setHeader('Content-Type', 'application/json');
+    res.send("Got ticketId: " + ticketId);
+});
+
+// GET total number of tickets.
+router.get('/tickets/count', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send("Current ticket count: 0");
+});
+
+// GET tickets on the given page number.
+router.get('/ticketsOnPage/:pageNumber', function (req, res) {
+    const pageNumber = parseInt(req.params.pageNumber);
+    res.setHeader('Content-Type', 'application/json');
+    res.send("Got page number: " + pageNumber);
 });
 
 module.exports = router;
