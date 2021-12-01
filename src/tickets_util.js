df86b07fda629798e27ab7569ffc000c3dc22e42
Sanket Sapkal <sanket@cursorinsight.com>
2021-11-30 22:31:27 -0800

    Added error handling when Zendesk is unreachable.

--- src/tickets_util.js
+++ src/tickets_util.js
@@ -22,6 +22,7 @@ function TicketsUtil() {}
 // Return all the tickets from Zendesk org.
 TicketsUtil.prototype.getAllTickets = function (res) {
     zendesk.tickets.list().then(function (tickets) {
+        checkError(tickets, res);
         res.end(JSON.stringify(tickets));
     }).catch(function (error) {
         console.error(error);
@@ -36,6 +37,7 @@ TicketsUtil.prototype.getTicketsOnPage = function (pageNumber, res) {
     // Fetch all the tickets from zendesk account and return only a slice of the
     // list
     zendesk.tickets.list().then(function (tickets) {
+        checkError(tickets, res);
         res.end(JSON.stringify(tickets.slice(firstTicketIndex,
                                              firstTicketIndex + ticketsPerPage)));
     }).catch(function (error) {
@@ -45,16 +47,28 @@ TicketsUtil.prototype.getTicketsOnPage = function (pageNumber, res) {
 
 TicketsUtil.prototype.getTicket = function(ticketId, res) {
     zendesk.tickets.show(ticketId).then(function (ticket) {
+        checkError(ticket, res);
         res.end(JSON.stringify(ticket));
     }).catch(console.error);
 }
 
 TicketsUtil.prototype.getTicketCount = function(res) {
     zendesk.tickets.list().then(function (tickets) {
+        checkError(tickets, res);
         res.end(JSON.stringify({'ticketCount': tickets.length}));
     }).catch(function (error) {
         console.error(error);
     });
+}
+
+function checkError(returnedResult, res) {
+    if(returnedResult === undefined || returnedResult === null) {
+        const log = JSON.stringify({'error' : 'Unable to fetch data from Zendesk'});
+        console.log("'error' : 'Unable to fetch data from Zendesk'");
+        res.writeHead(400, log, { 'content-type': 'application/json' });
+        res.end(log);
+        return;
+    }
 }
 
 module.exports = TicketsUtil;
